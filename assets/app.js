478756a8d20fbfcc756822b73c9c4d9a64c55ab2
Kemal Arda Girgin <kemalardagirgin34611@gmail.com>
2026-04-10 19:11:58 +0300

    Proje bitirildi son kodlar eklendi değişlikil için pull request.

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,58 @@
 function hesapla(){
-    var kilo,yas,boy,sonuc,cinsiyet;
-    kilo = Number(document.getElementById("boy").value);
+    var kilo,yas,boy,sonuc,cinsiyet,bmi,meter,durum;
+    kilo = Number(document.getElementById("kilo").value);
     yas = Number(document.getElementById("yas").value);
     boy = Number(document.getElementById("boy").value);
     cinsiyet = document.querySelector('input[name="cinsiyet"]:checked')?.value;
+    document.getElementById("sonuc").innerHTML="";
+
+    
+    
 
 
-    document.getElementById("sonuc").innerHTML="boy="+boy+"<br>"+"yaş="+yas+"<br>"+"kilo="+kilo+"<br>"+"cinsiyet="+cinsiyet;
+    if(cinsiyet=="" || boy=="" || yas=="" || kilo==""){
+        alert("Lütfen boş yer bırakmayınız");
+    }
+    else if(boy <=0 || boy >=250){
+        alert("lütfen 0 ve 250 arası pozitif bir değer giriniz.");
+    }
+    else if(yas <=0 || yas >=100 ){
+        alert("lütfen 0 ve 100 arası pozitif bir değer giriniz.");
+    }
+    else if(kilo <=0 || kilo >=300){
+        alert("lütfen 0 ve 300 arası pozitif bir değer giriniz");
+    }
+    else{
+        meter = boy / 100;
+        bmi = kilo / (meter * meter);
 
+        
+
+        if(bmi < 18.5){
+            durum = "Zayıf";
+        }
+        else if(bmi < 25){
+            durum = "Normal";
+        }
+        else if(bmi < 30){
+            durum = "Fazla Kilolu";
+        }
+        else{
+            durum = "Obez"
+        }
+
+        document.getElementById("sonuc").innerHTML=
+        "BMI: " +bmi+"<br>"+
+        "Durum: " + durum + "<br>"+
+        "Yaş: " + yas +"<br>"+
+        "Kilo: "+ kilo + "<br>"+
+        "Boy: " + boy + "<br>" +
+        "Cinsiyet: "+ cinsiyet + "<br>"; 
+        
+        
+    }
 
     
 }
+
+
